08b3f966871c723965e92a59dc6cc40e9d28c17a
Fanoussee <stephanie.t16@laposte.net>
2021-08-16 20:09:07 +0200

    Ajout de la suppression d'un ou de plusieurs articles et résolution de l'erreur d'unicité de la key dans Cart. Ajout d'une mise en forme css.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,29 +1,41 @@
 import "../styles/cart.css";
-import { useState } from "react";
+//import { useState } from "react";
 
 function Cart({ cart, updateCart }) {
-  const [isOpen, setIsOpen] = useState(false);
+  //const [isOpen, setIsOpen] = useState(false);
 
   const total = cart.reduce(
     (acc, plantType) => acc + plantType.amount * plantType.price,
     0
   );
 
-  return isOpen ? (
+  function deletePlant(index){
+    const basket = JSON.parse(localStorage.getItem("cart"));
+    const plantToDelete = basket[index];
+    if(plantToDelete.amount > 1){
+      basket[index].amount -= 1;
+      localStorage.setItem("cart", JSON.stringify(basket));
+    }else{
+      basket.splice(index, 1);
+      localStorage.setItem("cart", JSON.stringify(basket));
+    }
+  } 
+
+  return (
     <div className="lmj-cart">
-      <div className="btn-box">
-        <button className="btn-cart" onClick={() => setIsOpen(false)}>
-          Fermer
-        </button>
-      </div>
-      <h2>Panier</h2>
+      <h2 className="title-cart">Panier</h2>
       {cart.map(({ name, price, amount }, index) => (
-        <div className="ligne-article">
-          <div key={`${name}-${index}`}>
-            {name} {price}€ x {amount}
+        <div key={`${name}-${index}`} className="ligne-article">
+          <div>
+            {name} - quantité : {amount} - prix unitaire : {price}€ - total : {amount * price}€ 
           </div>
           <div className="btn-box">
-            <button className="btn-cart btn-suppr-article">Supprimer</button>
+            <button 
+              className="btn-cart btn-suppr-article"
+              onClick={() => {
+                deletePlant(index);
+                updateCart(JSON.parse(localStorage.getItem("cart")));
+              }}>Supprimer</button>
           </div>
         </div>
       ))}
@@ -40,13 +52,49 @@ function Cart({ cart, updateCart }) {
         </button>
       </div>
     </div>
-  ) : (
-    <div className="lmj-cart btn-box">
-      <button className="btn-cart" onClick={() => setIsOpen(true)}>
-        Ouvrir le panier
-      </button>
-    </div>
   );
+
+  // return isOpen ? (
+  //   <div className="lmj-cart">
+  //     <div className="btn-box">
+  //       <button className="btn-cart" onClick={() => setIsOpen(false)}>
+  //         Fermer
+  //       </button>
+  //     </div>
+  //     <h2>Panier</h2>
+  //     {cart.map(({ name, price, amount }, index) => (
+  //       <div className="ligne-article">
+  //         <div key={`${name}-${index}`}>
+  //           {name} {price}€ x {amount}
+  //         </div>
+  //         <div className="btn-box">
+  //           <button className="btn-cart btn-suppr-article">Supprimer</button>
+  //         </div>
+  //       </div>
+  //     ))}
+  //     <h3>Total : {total}€</h3>
+  //     <div className="btn-box">
+  //       <button
+  //         className="btn-cart"
+  //         onClick={() => {
+  //           updateCart([]);
+  //           localStorage.clear();
+  //         }}
+  //       >
+  //         Vider le panier
+  //       </button>
+  //     </div>
+  //   </div>
+  // ) : (
+  //   <div className="lmj-cart">
+  //     <div className="btn-box">
+  //       <button className="btn-cart" onClick={() => setIsOpen(true)}>
+  //         Ouvrir le panier
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
+  
 }
 
 export default Cart;
